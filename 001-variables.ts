59866d1d0dd6d73effdd16a73b78839af91c200d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-27 13:05:12 +0330

    Fix

--- 001-variables.ts
+++ 001-variables.ts
@@ -10,30 +10,3 @@ let friends: string[] = ["Reza", "Sina", "Kasra"]; // Array of strings
 
 let x: any = "Amirhossein"; // This var can be anything
 let xArr: any[] = ["Amir", 24, true, null, [], {}]; // This array can have anything
-
-// Functions
-
-const concatStrings = (a: string, b: string): string => {
-  // This function get 2 variables that both are string
-  // It returns always string
-
-  return a + b;
-};
-
-const plusNumber = (a: number, b: number): number => {
-  // This function get 2 variables that both are number
-  // It returns always number
-
-  return a + b;
-};
-
-const nameAge = (name: string, age: number): string => {
-  // This function get a name as string and age as number
-  // It returns a hi string
-
-  return `${name} has ${age} years old!`;
-};
-
-console.log(concatStrings("Hello ", "Amir"));
-console.log(plusNumber(2, 22));
-console.log(nameAge("Amirhossein", 24));
